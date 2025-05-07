a910ddd8ae53ebf2bdb88be626db0c7b435bc75f
Sairaj <sairajdusane@gmail.com>
2025-05-07 15:19:38 +0530

    implemerted interview generation

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
     <>
         <h3>Interview Generation</h3>
 
-        <Agent userName="You" userId="useer1" type="generate"/>
+        <Agent userName="You" userId="user1" type="generate"/>
 
     </> 
   )
